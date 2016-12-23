0cf9966081597252cda6a73ded8b2157380f3d6f
dotexe0 <dv@Daniels-MacBook-Pro.local>
2016-12-22 19:34:48 -0800

    functional chatroom. Added connected/disconnect broadcasting events

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   var input = $('input');
   var messages = $('#messages');
+  var socket = io();
 
   var addMessage = function(message) {
     messages.append('<div>' + message + '</div>')
@@ -10,9 +11,35 @@ $(document).ready(function() {
     if(event.keyCode != 13){
       return;
     }
-    
+
     var message = input.val();
     addMessage(message);
+    socket.emit('message', message);
     input.val('');
   });
+
+  var broadcastConnected = function() {
+    $('#connected').append('<p id="user">New user connected. </p>');
+    // $('#connected').appened('<p id="numOfUsers">'+ usersConnected + ' people here!</p>');
+    // console.log(usersConnected);
+    setTimeout(function(){
+      if ($('#user').length > 0) {
+        $('#user').remove();
+        // $('#numOfUsers').remove();
+      }
+    }, 2000);
+  };
+
+  var broadcastDisconnect = function() {
+    $('#connected').append('<p id="user">User disconnected</p>');
+    setTimeout(function(){
+      if ($('#user').length > 0) {
+        $('#user').remove();
+      }
+    }, 2000);
+  };
+
+  socket.on('message', addMessage);
+  socket.on('connected', broadcastConnected);
+  socket.on('disconnect', broadcastDisconnect);
 });
--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 var express = require('express');
+var socket_io = require('socket.io');
+var http = require('http');
 
 var app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+var server = http.Server(app);
+var io = socket_io(server);
+
+var usersConnected = 0;
+io.on('connection', function(socket) {
+  usersConnected += 1;
+  console.log('Client connected, ' + usersConnected + ' users here.');
+  socket.broadcast.emit('connected');
+
+  socket.on('disconnect', function(socket) {
+    usersConnected -= 1;
+    console.log('Client disconnected, ' + usersConnected + ' users here.');
+    io.emit('disconnect');
+  });
+
+  socket.on('message', function(message) {
+    console.log('Received message: ', message);
+    socket.broadcast.emit('message', message); // sends msg to all clients connected.
+  });
+});
+
+
+
+server.listen(process.env.PORT || 8080);
 console.log("Running on localhost:8080...");
